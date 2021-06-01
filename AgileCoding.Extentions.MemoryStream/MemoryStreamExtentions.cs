dd48b3f8488b0818b29d7f188de135550035f1a8
Ernie <gunning.ernie@gmail.com>
2021-06-01 17:01:13 +0200

    Project Start

---
+++ AgileCoding.Extentions.MemoryStream/MemoryStreamExtentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AgileCoding.Extentions.Streams
+{
+    public static class MemoryStreamExtentions
+    {
+        public static MemoryStream GetStreamForString(this MemoryStream stream, string data) 
+        {
+            stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.Write(data);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ ReadMePage.md
@@ -0,0 +1,5 @@
+﻿#AgileCoding.Extentions.MemoryStream
+
+###Extensions related to MemoryStream
+
+1.	MemoryStreamExtentions - Static class containing MemoryStream extentions
